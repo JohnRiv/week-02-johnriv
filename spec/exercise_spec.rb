d6d6a0d35546b9638307e80f6b064dbdd8be3c40
John Riviello <github@riviello.net>
2012-01-10 12:46:12 -0500

    Added a 2nd working test

--- spec/exercise_spec.rb
+++ spec/exercise_spec.rb
@@ -1,7 +1,14 @@
 describe "String" do
+  # simple test
   context "When calling .upcase" do
     it "Returns a copy of the string with all lowercase letters replaced with their uppercase counterparts" do
       "hEllO".upcase.should eq "HELLO"
+    end
+  end
+  # slightly more challenging test
+  context "When calling .succ (successor) on a 'z'" do
+    it "the 'z' becomes an 'a' and increments the character to the left of it" do
+      "zzz".succ.should eq "aaaa"
     end
   end
 end
